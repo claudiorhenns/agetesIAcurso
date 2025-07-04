545c7b53b79e0987b5460b38426529278ba9e457
jenifer-rhenns <jenifer.rhenns@gmail.com>
2025-07-04 12:37:17 -0300

    exercicio chat

--- intro-lang.py
+++ intro-lang.py
@@ -7,11 +7,22 @@ load_dotenv()
 
 
 llm = OpenAI()
-
+"""
 question = "me conte um historia de 200 palavras sobre a capital do Brasil"
 
 for trecho in llm.stream(question):
     print(trecho, end="")
+"""
 
+############
 #respostas = llm.invoke(question)
 #print(respostas)
+
+perguntas =[
+    'qual a maior torcida do Brasil?',
+    'quem foi pelé?',
+    'quem é o atual campeao brasileiro de futebol?'
+]
+
+respostas = llm.batch(perguntas)
+print(respostas)
